b886f85f42ba9eb0d79863ed9d8842d5f95bf84a
sankeerth1 <31911565+sankeerth1@users.noreply.github.com>
2017-09-29 15:07:16 -0500

    rotten tomatoes
    rotten tomatoes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,5 @@
 class MoviesController < ApplicationController
 
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
-
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -11,7 +7,14 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    sort = params[:sort]
+    order = params[:order]
+    @order = (order.nil? || order == 'desc') ? 'asc' : 'desc'
+
+    @all_ratings = Movie.all_ratings
+    ratings = params[:ratings]
+    @ratings = ratings.nil? ? Movie.all_ratings : ratings.keys
+    @movies = Movie.order("#{sort} #{order}").where('rating IN (?)', @ratings).all
   end
 
   def new
@@ -19,7 +22,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(movie_params)
+    @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -30,7 +33,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
+    @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
